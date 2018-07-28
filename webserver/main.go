8c98cc23ed8c517d147c28113d7c23a56340c723
jiangqianghua <jiangqianghua@jiangqianghuadeMacBook-Pro.local>
2018-07-28 15:22:43 +0800

    run gobuild.go1

--- webserver/main.go
+++ webserver/main.go
@@ -6,7 +6,7 @@ import(
 )
 
 func firstPage(w http.ResponseWriter , r *http.Request){
-	io.WriteString(w,"my first page")
+	io.WriteString(w,"my first page change1")
 }
 func main(){
 	http.HandleFunc("/",firstPage)
